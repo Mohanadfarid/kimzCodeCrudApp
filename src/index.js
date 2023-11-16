13b24fffccac4a3ee16b090bcef09b850f060c4d
Mohanad Farid <106895779+Mohanadfarid@users.noreply.github.com>
2023-11-16 12:58:05 +0200

    customized error page to get error when the user try to access post rout  with string

--- src/index.js
+++ src/index.js
@@ -2,31 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter ,RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { RootLayout } from "./Pages/RootLayout";
-import AddPost from "./Pages/AddPost"
-import EditPost from "./Pages/EditPost"
+import AddPost from "./Pages/AddPost";
+import EditPost from "./Pages/EditPost";
 import PostDetails from "./Pages/PostDetails";
 import Index from "./Pages/Index";
 import ErrorPage from "./Pages/ErrorPage";
 const router = createBrowserRouter([
-  {path:"/",
-  element:<RootLayout/>,
-  errorElement:<ErrorPage/>,
-  children:[
-    {index:true,element:<Index/>},
-    {path:"post",element:<Index/>},
-    {path:"post/add",element:<AddPost/>},
-    {path:"post/:id",element:<PostDetails/>},
-    {path:"post/:id/edit",element:<EditPost/>},
-
-  ]
-}
-])
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Index /> },
+      { path: "post", element: <Index /> },
+      { path: "post/add", element: <AddPost /> },
+      {
+        path: "post/:id",
+        element: <PostDetails />,
+        loader: ({params}) => {
+          if(isNaN(params.id)){
+            throw new Response("Bad Request", {statusText:"please make sure to insert correct post id",status:400})
+          }
+        },
+      },
+      { path: "post/:id/edit", element: <EditPost /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
